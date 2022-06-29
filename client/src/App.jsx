135a7a3e0fe0604934202cfbbdee8c179fde6310
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-06-29 15:28:44 -0500

    client registers successful connection through socket

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import io from 'socket.io-client'
 import './App.css'
 
 function App() {
-  const [text, setText] = useState("Awaiting things...");
   const [socket, setSocket] = useState(null);
+  const [handle, setHandle] = useState(null);
 
   useEffect(() => {
     const newSocket = io(`http://localhost:8080`);
@@ -14,24 +14,28 @@ function App() {
   }, [setSocket]);
 
   useEffect(() => {
-    console.log(socket);
-  }, [socket]);
+    if (!socket || !socket) return;
 
-  const getThings = async () => {
-    const response = await fetch("http://localhost:8080");
-    const json = await response.json();
-    const value = json.text;
-    
-    setText(text == value ? "Things are undone." : value);
-  }
+    socket.on('connect', () => {
+      console.log('connected!');
+      setHandle(socket.id);
+    })
+  }, [socket, setSocket]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
-        <button onClick={getThings}>Things?</button>
-        <p>{text}</p>
+
+        { socket ?
+        <>
+        <p>Socket!</p>
+        <p>User ID: {handle || "not found"}</p>
+        </>
+        : 
+        <p>{'No socket :('}</p>
+        }
       </header>
     </div>
   )
